4a144a178c2287ad0d737f2854219d2f8af55f3e
samboso <thechildrenlevis@gmail.com>
2018-10-11 09:49:01 +0100

    Probando la configuración de GitHub by Fabio

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -11,12 +11,11 @@ package javaapplication3;
  */
 public class JavaApplication3 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hola mundo");
+        System.out.print("Adiós mundo");
     }
     
 }
