5d68d79f52b64bd8ced118e265d46590590be7ad
samboso <thechildrenlevis@gmail.com>
2018-10-11 13:19:01 +0100

    Probando la configuración de GitHub by Fabio 2.0

---
+++ src/gitfabio2/GitFabio2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitfabio2;
+
+/**
+ *
+ * @author Fabio no
+ */
+public class GitFabio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Adiós mundo cruel");
+    }
+    
+}
