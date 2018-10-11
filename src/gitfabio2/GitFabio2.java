431b45cc2b5c5ebb13bb25dfbbb3712a0e816f2a
samboso <thechildrenlevis@gmail.com>
2018-10-11 13:24:09 +0100

    Probando la configuración de GitHub by Fabio 3.1

--- src/gitfabio2/GitFabio2.java
+++ src/gitfabio2/GitFabio2.java
@@ -15,7 +15,7 @@ public class GitFabio2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Adiós mundo cruel!!!");
+        System.out.println("¡¡¡¡Adiós mundo cruel!!!");
     }
     
 }
